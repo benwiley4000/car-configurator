5ad47e0974749056fdc41a1558dbce95430dab62
Houssem <houssem@3dverse.com>
2023-06-14 14:45:09 -0400

    feat: urus and viper selection, front/rear bumper and spoiler selection working

--- public/AppConfig.js
+++ public/AppConfig.js
@@ -1,4 +1,37 @@
 AppConfig = {
   sceneUUID: "060594e9-3255-4329-805a-b448bc8605c0",
   cameraComponentDataJSON: {},
+  cars: [
+    {
+      name: "Viper",
+      sceneUUID: "6d4481b2-69fb-4521-ac39-47f3a9ad7c7e",
+      frontBumpers: [
+        "c65d9334-d97f-445f-a0be-519dffcd5126",
+        "64f37981-90e2-48fd-a901-a7b56abc15c6",
+        "b39db704-77a0-45ed-a7cc-2362570db366",
+      ],
+      rearBumpers: [],
+      spoilers: [
+        "7597ade2-d54a-43e2-89c0-b8c2315e6bd3",
+        "9d705356-17fd-48ea-a87f-84b4dd401690",
+        "32cdda8e-84d7-45c8-b68b-e825f7f4fb8b",
+      ],
+    },
+    {
+      name: "Urus",
+      sceneUUID: "0c6a9f74-feb4-4aed-8f59-463c24da1b2f",
+      frontBumpers: [
+        "b9f97280-ee11-4071-a1d7-71aa7c7a1b21",
+        "3e0989e4-2b6b-43b5-b080-6dd0e8cf5b5d",
+      ],
+      rearBumpers: [
+        "36a49530-3c1c-42b5-a5dc-0384943cc3e5",
+        "fa25eafb-8aff-4f7e-af08-0c9b72afe6e2",
+      ],
+      spoilers: [
+        "f294fc15-2573-4c92-a80d-c61fb7c82c61",
+        "5e5e1e23-66d6-46f7-a095-387001d568f6",
+      ],
+    },
+  ],
 };
--- public/app.js
+++ public/app.js
@@ -1,9 +1,30 @@
 //--------------------------------------------------------------------------------------------------
 window.addEventListener("load", InitApp);
 
+let gCarAttachment = null;
+let gSelectedCar = null;
+let carParts = {
+  body: null,
+  frontBumper: null,
+  rearBumper: null,
+  spoiler: null,
+};
+
 //--------------------------------------------------------------------------------------------------
 async function InitApp() {
-  SDK3DVerse.setViewports(null);
+  //SDK3DVerse.setViewports(null);
+
+  const viewports = [
+    {
+      id: 0,
+      left: 0,
+      top: 0,
+      width: 1,
+      height: 1,
+      defaultControllerType: 1,
+    },
+  ];
+  SDK3DVerse.setViewports(viewports);
   SetResolution();
 
   let debounceResizeTimeout = null;
@@ -30,6 +51,109 @@ async function InitApp() {
       "start_simulation"
     );
   }
+
+  FadeOut();
+  SetInformation("");
+  await InitCarAttachment();
+
+  await ChangeCar({ value: 0 });
+}
+
+//--------------------------------------------------------------------------------------------------
+async function InitCarAttachment() {
+  [gCarAttachment] = await SDK3DVerse.engineAPI.findEntitiesByNames(
+    "CAR_ATTACHMENT"
+  );
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ChangeCar(e) {
+  gSelectedCar = AppConfig.cars[e.value];
+  await RemoveExistingCar();
+  await ApplySelectedCar();
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ChangeSpoiler(e) {
+  const i = e.value;
+  if (i >= gSelectedCar.spoilers.length) {
+    return;
+  }
+
+  carParts.spoiler = await ChangePart(
+    carParts.spoiler,
+    gSelectedCar.name + " SPOILER " + i,
+    gSelectedCar.spoilers[i]
+  );
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ChangeFrontBumper(e) {
+  const i = e.value;
+  if (i >= gSelectedCar.frontBumpers.length) {
+    return;
+  }
+
+  carParts.frontBumper = await ChangePart(
+    carParts.frontBumper,
+    gSelectedCar.name + " FRONT BUMPER " + i,
+    gSelectedCar.frontBumpers[i]
+  );
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ChangeRearBumper(e) {
+  const i = e.value;
+  if (i >= gSelectedCar.rearBumpers.length) {
+    return;
+  }
+
+  carParts.rearBumper = await ChangePart(
+    carParts.rearBumper,
+    gSelectedCar.name + " REAR BUMPER " + i,
+    gSelectedCar.rearBumpers[i]
+  );
+}
+
+//--------------------------------------------------------------------------------------------------
+async function RemoveExistingCar() {
+  const children = await SDK3DVerse.engineAPI.getEntityChildren(gCarAttachment);
+  await SDK3DVerse.engineAPI.deleteEntities(children);
+
+  carParts.body = null;
+  carParts.frontBumper = null;
+  carParts.rearBumper = null;
+  carParts.spoiler = null;
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ApplySelectedCar() {
+  carParts.body = await ChangePart(
+    carParts.body,
+    gSelectedCar.name,
+    gSelectedCar.sceneUUID
+  );
+  await ChangeFrontBumper({ value: 0 });
+  await ChangeRearBumper({ value: 0 });
+  await ChangeSpoiler({ value: 0 });
+}
+
+//--------------------------------------------------------------------------------------------------
+async function ChangePart(part, name, partUUID) {
+  if (part !== null) {
+    await SDK3DVerse.engineAPI.deleteEntities([part]);
+  }
+
+  return await SelectPart(name, partUUID);
+}
+
+//--------------------------------------------------------------------------------------------------
+async function SelectPart(partName, partSceneUUID) {
+  const part = { debug_name: { value: partName } };
+  SDK3DVerse.utils.resolveComponentDependencies(part, "scene_ref");
+
+  part.scene_ref.value = partSceneUUID;
+  return await SDK3DVerse.engineAPI.spawnEntity(gCarAttachment, part);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -105,10 +229,10 @@ async function Connect() {
   const connectionInfo = await SDK3DVerse.webAPI.createOrJoinSession(
     AppConfig.sceneUUID
   );
+  connectionInfo.useSSL = true;
   SDK3DVerse.setupDisplay(document.getElementById("display_canvas"));
   SDK3DVerse.startStreamer(connectionInfo);
   await SDK3DVerse.connectToEditor();
-
   SetInformation("Connection to 3dverse established...");
   return connectionInfo.sessionCreated;
 }
