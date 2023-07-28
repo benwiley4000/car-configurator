87535c5d316780e6947b57f4805bfed7201d062d
Matéo <mateo@3dverse.com>
2023-07-28 11:01:47 +0200

    fix: grab speed on mobile

--- public/app.js
+++ public/app.js
@@ -608,7 +608,7 @@ function onMediaQueryChange(e) {
         -0.08518092334270477, -0.2508307993412018, -0.02216341346502304,
         0.9640212059020996,
       ],
-      SDK3DVerse.updateControllerSetting({ speed: 1, sensitivity: 1 })
+      SDK3DVerse.updateControllerSetting({ speed: 1, sensitivity: 0.8 })
     );
   } else {
     console.log("> 768px");
