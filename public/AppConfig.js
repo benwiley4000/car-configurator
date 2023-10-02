780081957994b96d8fe3d469760afbf46e7cc34c
benwiley3d <ben@3dverse.com>
2023-10-02 15:50:26 -0400

    fix

--- public/AppConfig.js
+++ public/AppConfig.js
@@ -1,97 +1,104 @@
-/** @global */ // prettier-ignore
-const AppConfig =
-{
-  "apiUrl": "https://api.3dverse.com/app/v1",
-  "sceneUUID": "060594e9-3255-4329-805a-b448bc8605c0",
-  "cubemaps": [
+/** @global */
+const AppConfig = {
+  apiUrl: "https://api.3dverse.com/app/v1",
+  sceneUUID: "060594e9-3255-4329-805a-b448bc8605c0",
+  cubemaps: [
     {
-      "name": "Pure Sky",
-      "skyboxUUID": "eb6b6cfc-a25f-4f9b-9cbf-287488a5f902",
-      "radianceUUID": "2abf3b02-7ce9-437c-a85f-5f2f54ecc67b",
-      "irradianceUUID": "ff345697-eca6-4970-bec7-7e6b1d52c715"
+      name: "Pure Sky",
+      skyboxUUID: "eb6b6cfc-a25f-4f9b-9cbf-287488a5f902",
+      radianceUUID: "2abf3b02-7ce9-437c-a85f-5f2f54ecc67b",
+      irradianceUUID: "ff345697-eca6-4970-bec7-7e6b1d52c715",
     },
     {
-      "name": "Zwinger Night",
-      "skyboxUUID": "3d87dcf8-7e9e-40b2-9187-521d6a2f0d7a",
-      "radianceUUID": "2312C1F1-3277-4DDE-8D74-6F42B413F447",
-      "irradianceUUID": "e8308b91-206b-4314-b1de-5d9831534fc8"
+      name: "Zwinger Night",
+      skyboxUUID: "3d87dcf8-7e9e-40b2-9187-521d6a2f0d7a",
+      radianceUUID: "2312C1F1-3277-4DDE-8D74-6F42B413F447",
+      irradianceUUID: "e8308b91-206b-4314-b1de-5d9831534fc8",
     },
     {
-      "name": "Blouberg Sunrise",
-      "skyboxUUID": "3d73a47a-ade0-43a8-bfe3-ac9488d79d59",
-      "radianceUUID": "5cb7653e-fbf1-497a-bd64-26ad617ee6aa",
-      "irradianceUUID": "2f957174-2439-444f-95ed-5409a2c7a7b5"
-    }
+      name: "Blouberg Sunrise",
+      skyboxUUID: "3d73a47a-ade0-43a8-bfe3-ac9488d79d59",
+      radianceUUID: "5cb7653e-fbf1-497a-bd64-26ad617ee6aa",
+      irradianceUUID: "2f957174-2439-444f-95ed-5409a2c7a7b5",
+    },
   ],
-  "cameraComponentDataJSON": {},
-  "materials": [
+  colorChoices: /** @type {[number, number, number][]} */ ([
+    [0, 0.369, 0.302],
+    [0.467, 0.51, 0],
+    [0, 0.035, 0.29],
+    [0.58, 0.141, 0.506],
+    [0.251, 0, 0],
+  ]),
+  materials: [
     {
-      "name": "Solid",
-      "matUUID": "bd9dc4c5-20b7-404c-8a94-b84cf21de580"
+      name: "Solid",
+      matUUID: "bd9dc4c5-20b7-404c-8a94-b84cf21de580",
     },
     {
-      "name": "Metallic",
-      "matUUID": "e1357fe8-7a6f-4af5-83e0-b16643b98372"
+      name: "Metallic",
+      matUUID: "e1357fe8-7a6f-4af5-83e0-b16643b98372",
     },
     {
-      "name": "Matte",
-      "matUUID": "8585675f-5dad-4b3a-904d-3afe8cbf0c03"
-    }
+      name: "Matte",
+      matUUID: "8585675f-5dad-4b3a-904d-3afe8cbf0c03",
+    },
   ],
-  "cars": [
+  cars: [
     {
-      "name": "Dodge Viper SRT",
-      "description": "The legend became infamous after ex-Top Gear presenter Richard Hammond crashed one of only eight cars build.",
-      "price": "346 000$",
-      "sceneUUID": "6d4481b2-69fb-4521-ac39-47f3a9ad7c7e",
-      "paintMaterialUUID": "85f74efa-1f3e-4ddb-8b16-dad907d2ecad",
-      "frontBumpers": [
+      name: "Dodge Viper SRT",
+      description:
+        "The legend became infamous after ex-Top Gear presenter Richard Hammond crashed one of only eight cars built.",
+      price: "346 000$",
+      sceneUUID: "6d4481b2-69fb-4521-ac39-47f3a9ad7c7e",
+      paintMaterialUUID: "85f74efa-1f3e-4ddb-8b16-dad907d2ecad",
+      frontBumpers: [
         "c65d9334-d97f-445f-a0be-519dffcd5126",
         "64f37981-90e2-48fd-a901-a7b56abc15c6",
-        "b39db704-77a0-45ed-a7cc-2362570db366"
+        "b39db704-77a0-45ed-a7cc-2362570db366",
       ],
-      "rearBumpers": [],
-      "spoilers": [
+      rearBumpers: [],
+      spoilers: [
         "7597ade2-d54a-43e2-89c0-b8c2315e6bd3",
         "9d705356-17fd-48ea-a87f-84b4dd401690",
-        "32cdda8e-84d7-45c8-b68b-e825f7f4fb8b"
+        "32cdda8e-84d7-45c8-b68b-e825f7f4fb8b",
       ],
-      "headLightsMatUUID": "6062958a-4e07-4fa9-929f-3455d5160f4f",
-      "rearLightsMatUUID": "ca52ec66-ec04-43ca-bb39-0f2e6055e7bb",
-      "maxSpeed": "380",
-      "acceleration": "2.1",
-      "maximumPower": "962",
-      "maximumTorque": "932",
-      "engineCapacity": "4.3"
+      headLightsMatUUID: "6062958a-4e07-4fa9-929f-3455d5160f4f",
+      rearLightsMatUUID: "ca52ec66-ec04-43ca-bb39-0f2e6055e7bb",
+      maxSpeed: "380",
+      acceleration: "2.1",
+      maximumPower: "962",
+      maximumTorque: "932",
+      engineCapacity: "4.3",
     },
     {
-      "name": "Lamborghini Urus",
-      "description": "Lamborghini Urus is the first Super Sport Utility Vehicle in the world, merging the soul of a super sports car with the practical functionality of an SUV.",
-      "price": "228 000$",
-      "sceneUUID": "0c6a9f74-feb4-4aed-8f59-463c24da1b2f",
-      "paintMaterialUUID": "20f3387f-b549-49ce-bf21-1fec67abef63",
-      "frontBumpers": [
+      name: "Lamborghini Urus",
+      description:
+        "Lamborghini Urus is the first Super Sport Utility Vehicle in the world, merging the soul of a super sports car with the practical functionality of an SUV.",
+      price: "228 000$",
+      sceneUUID: "0c6a9f74-feb4-4aed-8f59-463c24da1b2f",
+      paintMaterialUUID: "20f3387f-b549-49ce-bf21-1fec67abef63",
+      frontBumpers: [
         "b9f97280-ee11-4071-a1d7-71aa7c7a1b21",
-        "3e0989e4-2b6b-43b5-b080-6dd0e8cf5b5d"
+        "3e0989e4-2b6b-43b5-b080-6dd0e8cf5b5d",
       ],
-      "rearBumpers": [
+      rearBumpers: [
         "36a49530-3c1c-42b5-a5dc-0384943cc3e5",
-        "fa25eafb-8aff-4f7e-af08-0c9b72afe6e2"
+        "fa25eafb-8aff-4f7e-af08-0c9b72afe6e2",
       ],
-      "spoilers": [
+      spoilers: [
         "f294fc15-2573-4c92-a80d-c61fb7c82c61",
-        "5e5e1e23-66d6-46f7-a095-387001d568f6"
+        "5e5e1e23-66d6-46f7-a095-387001d568f6",
       ],
-      "headLightsMatUUID": "d2de613c-9d0e-4612-b253-e8ea0740c960",
-      "rearLightsMatUUID": "ef8b7508-7ef3-47a9-9e5a-6ed6f039b62a",
-      "maxSpeed": "270",
-      "acceleration": "3.2",
-      "maximumPower": "1045",
-      "maximumTorque": "1234",
-      "engineCapacity": "12.6"
-    }
-  ]
-}
+      headLightsMatUUID: "d2de613c-9d0e-4612-b253-e8ea0740c960",
+      rearLightsMatUUID: "ef8b7508-7ef3-47a9-9e5a-6ed6f039b62a",
+      maxSpeed: "270",
+      acceleration: "3.2",
+      maximumPower: "1045",
+      maximumTorque: "1234",
+      engineCapacity: "12.6",
+    },
+  ],
+};
 
 // @ts-ignore
 window.AppConfig = AppConfig;
